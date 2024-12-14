6d13683fc8d5311e56231a71e6d2edaab9db25d9
ynsocial <38935680+ynsocial@users.noreply.github.com>
2024-12-14 03:58:48 +0300

    Delete routes directory

--- routes/web.php
+++
@@ -1,5 +0,0 @@
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
-
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show'); 
